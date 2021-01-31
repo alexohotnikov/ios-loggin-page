e9f4a571574bb583206da7de25488a7050930de1
Aleksandr Ohotnikov <aleksohotnikov@gmail.com>
2021-01-31 15:44:57 +0400

    Initial Commit

---
+++ loginPage/loginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginPageApp.swift
+//  loginPage
+//
+//  Created by Саша on 31.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct loginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
